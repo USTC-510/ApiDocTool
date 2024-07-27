7af53af7a63daa385c19c7d2f6dc17ef016657f3
Maiden1969 <zkr230527@mail.ustc.edu.cn>
2024-07-27 12:54:53 +0800

    最后的更新

--- src/main/java/annotations/Remark.java
+++ src/main/java/annotations/Remark.java
@@ -4,7 +4,11 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+/**
+ * @Author:   周康睿
+ * @Description:  为Api文档添加备注
+ * @Version:   1.0
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @SuppressWarnings("all")
